ba2597fa93a0ec5420229a9eaabae460a3297dc9
hendychua <hendychua@gmail.com>
2017-11-21 21:07:54 +0800

    Implement describe dimension.

--- config/project.go
+++ config/project.go
@@ -1,6 +1,14 @@
 package config
 
+import (
+  "fmt"
+)
+
 // Project is a struct that represents a project.
 type Project struct {
   ProjectPath string
+}
+
+func (p Project) String() string {
+  return fmt.Sprintf("ProjectPath: '%s'", p.ProjectPath)
 }
--- subcommands/describe_dimension.go
+++ subcommands/describe_dimension.go
@@ -2,11 +2,53 @@ package subcommands
 
 import (
   "fmt"
+  "errors"
+  "strings"
+
+  "github.com/hendychua/slingring/config"
 )
 
+// DescribeDimension is a command that describes a Dimension.
 type DescribeDimension struct{}
 
+// Run describes a Dimension with <name> in detail.
+// If such a Dimension does not exist, the command returns an error.
 func (d DescribeDimension) Run(args []string) error {
-  fmt.Println("DescribeDimension called with args: ", args)
+  if len(args) != 1 {
+    return errors.New("illegal usage: DeleteDimension takes in 1 argument")
+  }
+
+  name := strings.TrimSpace(strings.ToLower(args[0]))
+
+  if len(name) <= 0 {
+    return errors.New("illegal name for Dimension: Cannot be empty")
+  }
+
+  data, err := config.GetGlobalData()
+  if err != nil {
+    return err
+  }
+
+  if dimension, ok := data.Dimensions[name]; !ok {
+    return fmt.Errorf("fatal error: no Dimension named '%s'", name)
+  } else {
+    // TODO: formatting the output to align the left columns can be done in a better way.
+    fmt.Printf("Name:               %s\n", dimension.Name)
+    if strings.EqualFold(data.CurrentDimension, dimension.Name) {
+      fmt.Printf("Current default:    yes\n\n")
+    } else {
+      fmt.Printf("Current default:    no\n\n")
+    }
+    if len(dimension.Projects) > 0 {
+      fmt.Printf("Projects:\n")
+      for _, project := range dimension.Projects {
+        fmt.Printf("%s\n", project)
+      }
+    } else {
+      fmt.Printf("(No projects added to dimension.)\n")
+    }
+    fmt.Println()
+  }
+
   return nil
 }
