709fd5701f34a78b5c982e015c381fddc81956f7
hendychua <hendychua@gmail.com>
2017-11-21 21:54:28 +0800

    Add comments to add_project command.

--- subcommands/add_project.go
+++ subcommands/add_project.go
@@ -4,9 +4,14 @@ import (
   "fmt"
 )
 
+// AddProject is a command that adds a project path to a Dimension.
 type AddProject struct{}
 
-func (a AddProject) Run(currentDimension string, args []string) error {
-  fmt.Println("AddProject called with currentDimension: '", currentDimension, "', args: ", args)
+// Run adds a project path to a Dimension.
+// If dimensionToWorkOn happens to be the currently set dimension, this function
+// will also attempt to jump to the dimension for the newly added project.
+func (a AddProject) Run(dimensionToWorkOn string, args []string) error {
+  // TODO:
+  fmt.Println("AddProject called with dimensionToWorkOn: '", dimensionToWorkOn, "', args: ", args)
   return nil
 }
